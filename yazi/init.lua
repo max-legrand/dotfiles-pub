822fa0ed3a305098ac258fe63bf8eb7f1fc0bab4
Maxwell Legrand <max4home@yahoo.com>
2026-08-12 19:14:51 -0400

    Yazi + helix config

---
+++ yazi/init.lua
@@ -0,0 +1,10 @@
+function Entity:icon()
+	local icon = self._file:icon()
+	if not icon then
+		return ""
+	elseif self._file.is_hovered then
+		return " " .. icon.text .. "  "
+	else
+		return ui.Line(icon.text .. "  "):style(icon.style)
+	end
+end
